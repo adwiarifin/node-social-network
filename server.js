896add7a1751d415bbba98309b3466c0f01e898d
Adwi Arifin <adwiarifin@gmail.com>
2019-02-03 09:25:57 +0700

    setup route

--- server.js
+++ server.js
@@ -4,3 +4,11 @@ const server = new Hapi.Server();
 server.connection({port:3000});
 
 server.start(console.log("test"));
+
+server.route({
+    method: "GET",
+    path: "/",
+    handler: function(request, reply) {
+        reply("welcome to node connect");
+    }
+});
